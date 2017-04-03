bb821b360d5769ca84e98c5abb71b48cd3e15cf3
Adam Parker <amichaelparker@gmail.com>
2017-04-03 12:19:17 -0500

    added baseviewmodel, fixed todo items

--- src/TechJobs/Controllers/JobController.cs
+++ src/TechJobs/Controllers/JobController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 using TechJobs.Data;
+using TechJobs.Models;
 using TechJobs.ViewModels;
 
 namespace TechJobs.Controllers
@@ -19,8 +22,8 @@ namespace TechJobs.Controllers
         public IActionResult Index(int id)
         {
             // TODO #1 - get the Job with the given ID and pass it into the view
-
-            return View();
+            Job jobWithId = jobData.Find(id);
+            return View(jobWithId);
         }
 
         public IActionResult New()
@@ -35,8 +38,25 @@ namespace TechJobs.Controllers
             // TODO #6 - Validate the ViewModel and if valid, create a 
             // new Job and add it to the JobData data store. Then
             // redirect to the Job detail (Index) action/view for the new Job.
+            if (ModelState.IsValid)
+            {
+                JobData jobData = JobData.GetInstance();
 
-            return View(newJobViewModel);
+                Job newJob = new Job
+                {
+                    Name = newJobViewModel.Name,
+                    Employer = jobData.Employers.Find(newJobViewModel.EmployerID),
+                    Location = jobData.Locations.Find(newJobViewModel.LocationID),
+                    CoreCompetency = jobData.CoreCompetencies.Find(newJobViewModel.CoreCompetencyID),
+                    PositionType = jobData.PositionTypes.Find(newJobViewModel.PositionTypeID),
+                };
+                jobData.Jobs.Add(newJob);
+                return Redirect("/Job?id=" + newJob.ID);
+            } else
+            {
+                ViewBag.error = "Please enter a name for the job listing.";
+                return New();
+            }
         }
     }
 }
---
+++ src/TechJobs/ViewModels/BaseViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechJobs.Models;
+
+namespace TechJobs.ViewModels
+{
+    public class BaseViewModel
+    {
+        // The current column, initialized to All
+        public JobFieldType Column { get; set; } = JobFieldType.All;
+
+        // All columns, for display
+        public List<JobFieldType> Columns { get; set; }
+
+        //View Title
+        public string Title { get; set; } = "";
+
+        public void populateColumns()
+        {
+            Columns = new List<JobFieldType>();
+
+            foreach (JobFieldType enumVal in Enum.GetValues(typeof(JobFieldType)))
+            {
+                Columns.Add(enumVal);
+            }
+        }
+    }
+}
